b110b65aa5d0b645c5d94722d0d9a26b7fc46f1e
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-07-08 23:29:00 -0500

    user routes setup

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -3,32 +3,8 @@ import userModel, { IUser } from 'src/models/user.model';
 import ErrorHandler from 'src/utils/ErrorHandler';
 import { JwtGuard } from 'src/auth/jwt-auth.guard';
 
-interface IUpdateUser {
-    name?: string;
-    email?: string;
-}
 
 @Controller('user')
 export class UserController {
 
-    @UseGuards(JwtGuard)
-    @Get(':id')
-    async findOne(@Param('id') id: string, @Res() res, @Next() next) {
-        try {
-            const user = await userModel.findById(id);
-            res.status(201).json(user);
-        } catch (error: any) {
-            return next(new ErrorHandler(error.message, 400));
-        }
-    }
-    
-    @Put(':id')
-    async update(@Param('id') id: string, @Body() user: IUpdateUser, @Res() res, @Next() next) {
-        try {
-            await userModel.findByIdAndUpdate(id, user);
-            res.status(201).json(user);
-        } catch (error: any) {
-            return next(new ErrorHandler(error.message, 400));
-        }
-    }
 }
