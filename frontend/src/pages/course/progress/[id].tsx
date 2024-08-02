8abcf727f5cf71733b60fd8005e39ab77dde9bda
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-08-02 01:46:08 -0500

    fixed fetch endpoint

--- frontend/src/pages/course/progress/[id].tsx
+++ frontend/src/pages/course/progress/[id].tsx
@@ -5,29 +5,40 @@ import { useEffect, useState } from "react"
 const CourseProgress = () => {
     const router = useRouter()
     const { id } = router.query
-    const stringId = id?.toString()
     const [title, setTitle] = useState("")
 
     useEffect(() => {
+        if (!id) return;
+
         const fetchCourse = async () => {
             try {
-                const response = await fetch(`http://localhost:8000/content/${id}`);
+                const token = localStorage.getItem('access-token');
+                if (!token) throw new Error('No token found');
+
+                const response = await fetch(`http://localhost:8000/course/content/${id}`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+                if (!response.ok) {
+                    throw new Error('Course not found');
+                }
                 const data = await response.json();
                 console.log(data)
                 setTitle(data.name)
-              } catch (error) {
-                console.error('Error fetching courses:', error);
-              }
+            } catch (error) {
+                console.error('Error fetching course:', error);
+            }
         }
 
         fetchCourse()
-    }, [])
-
+    }, [id])
 
     return (
-      <>
-        <CourseProgressSidebar title={title} />
-      </>
+        <>
+            <CourseProgressSidebar title={title} />
+        </>
     )
 }
 
