13ec601e4c1895461a8786c99caec12f76e23ef5
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-08-26 19:30:38 -0500

    loading state

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -65,7 +65,9 @@ const UserProfile = () => {
                         </div>
                     </div>
                 ) : (
-                    <p className="text-center">Loading user profile...</p>
+                    <div className="flex justify-center">
+                        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-sky-700"></div>
+                    </div>
                 )}
             </div>
         </>
